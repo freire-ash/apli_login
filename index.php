d1c6b03f76016c39cb3be39075540d71ec33af64
ashuro-ryuki <lffxds@gmail.com>
2024-06-12 05:57:51 -0300

    adiciona o texto de introdução na pagina e tambem coloca os links do github e do linkedin

---
+++ index.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="en" class="h-100">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.84.0">
+    <title>Cover Template · Bootstrap v5.0</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/cover/">
+
+    
+
+    <!-- Bootstrap core CSS -->
+<link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+  (
+
+  )
+  
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="cover.css" rel="stylesheet">
+  </head>
+  <body class="d-flex h-100 text-center text-white bg-dark">
+    
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <header class="mb-auto">
+    <div>
+      <nav class="nav nav-masthead justify-content-center float-md-end">
+        <a class="nav-link active" aria-current="page" href="index.php">início</a>
+        <a class="nav-link" href="cadastro.php">cadastro</a>
+        <a class="nav-link" href="login.php">login</a>
+      </nav>
+    </div>
+  </header>
+
+  <main class="px-3">
+    <h1>sejam todos bem vindos❤️</h1>
+    <p class="lead">essa estilização é uma prosposta feita em sala de aula pelo leonardo rocha para aprendermos a conectar back-end com o front-end especificamente com o banco de dados, acesse esse repositorio no  </p>
+    <p class="lead">
+      <a href="https://github.com/freire-ash" class="btn btn-lg btn-secondary fw-bold border-white bg-white">github</a>
+    </p>
+  </main>
+
+  <footer class="mt-auto text-white-50">
+    <p>template feito para <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, por <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p>
+ <p>estilizacão feita por <a href="https://www.linkedin.com/in/freire-ash/" class="text-white">freire_ash</a>
+    </footer>
+</div>
+
+
+    
+  </body>
+</html>
